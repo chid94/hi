cb818ff428f2f12edffd1a91c75dd3398f93c831
chid94 <chidhuscr@gmail.com>
2025-06-01 17:22:51 +0530

    PythonSelenium

--- list1 = list((1,2,3)).py
+++
@@ -1,9 +0,0 @@
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = []
-print(fruits)
-# Git hub latest codesgit
-for x in fruits:
-  if "a" in x:
-    newlist.append(x)
-
-print(newlist)
